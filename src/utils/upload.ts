fb38a3c2c08b7c978c25664561e0c9e6c7d504ba
W7EVEN <emmanoelw@gmail.com>
2025-07-13 10:37:58 -0300

    fix: Corrects API route for upload

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -15,10 +15,13 @@ export const uploadImage = async (file: File): Promise<UploadResponse> => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch("http://localhost:3000/api/upload", {
-      method: "POST",
-      body: formData,
-    });
+    const response = await fetch(
+      "https://hackathon-ten-sable.vercel.app/api/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
     const result = await response.json();
 
